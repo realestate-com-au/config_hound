a5090bd135bfe418b60585642dccd2c14dea7b6b
Mike Williams <mdub@dogbiscuit.org>
2015-03-03 21:40:39 +1100

    Extract Parser.

--- lib/configuration_loader.rb
+++ lib/configuration_loader.rb
@@ -1,3 +1,4 @@
+require "configuration_loader/parser"
 require "configuration_loader/resource"
 require "configuration_loader/version"
 
@@ -9,7 +10,7 @@ class ConfigurationLoader
 
   def load(path)
     resource = Resource.new(path.to_s)
-    data = parse(resource.read, resource.format)
+    data = Parser.parse(resource.read, resource.format)
     includes = Array(data.delete("_include"))
     includes.each do |i|
       included_data = load(resource.resolve(i))
@@ -19,29 +20,6 @@ class ConfigurationLoader
   end
 
   private
-
-  def parse(raw, format)
-    parse_method = "parse_#{format}"
-    raise "unknown format: #{format}" unless respond_to?(parse_method, true)
-    send(parse_method, raw)
-  end
-
-  def parse_yaml(raw)
-    require "yaml"
-    YAML.load(raw)
-  end
-
-  alias :parse_yml :parse_yaml
-
-  def parse_json(raw)
-    require "multi_json"
-    MultiJson.load(raw)
-  end
-
-  def parse_toml(raw)
-    require "toml"
-    TOML.load(raw)
-  end
 
   def deep_merge(a, b)
     if a.is_a?(Hash) && b.is_a?(Hash)
---
+++ lib/configuration_loader/parser.rb
@@ -0,0 +1,36 @@
+class ConfigurationLoader
+
+  class Parser
+
+    def self.parse(*args)
+      new.parse(*args)
+    end
+
+    def parse(raw, format)
+      parse_method = "parse_#{format}"
+      raise "unknown format: #{format}" unless respond_to?(parse_method, true)
+      send(parse_method, raw)
+    end
+
+    protected
+
+    def parse_yaml(raw)
+      require "yaml"
+      YAML.load(raw)
+    end
+
+    alias :parse_yml :parse_yaml
+
+    def parse_json(raw)
+      require "multi_json"
+      MultiJson.load(raw)
+    end
+
+    def parse_toml(raw)
+      require "toml"
+      TOML.load(raw)
+    end
+
+  end
+
+end
