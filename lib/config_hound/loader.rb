4671525729b0f846e1bdc7e2756a58614b19ea90
Danial Pearce <danial.pearce@rea-group.com>
2021-07-11 17:54:09 +1000

    Pass the allow_dupes option through to the parser

--- lib/config_hound/loader.rb
+++ lib/config_hound/loader.rb
@@ -8,9 +8,11 @@ module ConfigHound
     DEFAULT_INCLUDE_KEY = "_include"
 
     attr_accessor :include_key
+    attr_accessor :allow_duplicate_keys
 
     def initialize(options = {})
       @include_key = DEFAULT_INCLUDE_KEY
+      @allow_duplicate_keys = true
       options.each do |k, v|
         public_send("#{k}=", v)
       end
@@ -26,7 +28,7 @@ module ConfigHound
     def load_source(source)
       return source if source.is_a?(Hash)
       resource = Resource[source]
-      raw_data = resource.load
+      raw_data = resource.load(allow_duplicate_keys: allow_duplicate_keys)
       includes = Array(raw_data.delete(include_key))
       included_resources = includes.map do |relative_path|
         resource.resolve(relative_path)
